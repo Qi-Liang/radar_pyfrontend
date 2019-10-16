a3e7184f7e34a7bfd73b365947975346d0b68fca
1325075688gw <1325075688@qq.com>
2019-10-16 22:01:16 +0800

    单帧数据测试

--- PortControl/data_process.py
+++ PortControl/data_process.py
@@ -4,15 +4,22 @@
 
 
 import json
+import numpy as np
 
 res = []
-with open(r'hi.json', 'r', encoding='utf-8') as f:
-    txt = json.load(f)
-    for i in txt:
-        t = []
-        for j in txt[i]:
-            tt = [j['x'], j['y'], j['z']]
-            t.append(tt)
-        res.append(t)
+test_data = {"1": [{"x": 2, "y": 4, "z": 5}, {"x": 3, "y": 4, "z": 2}, {"x": 1, "y": 7, "z": 4}],
+             "2": [{"x": 2, "y": 2, "z": 6}, {"x": 3, "y": 4, "z": 8}, {"x": 5, "y": 7, "z": 6}],
+             "3": [{"x": 5, "y": 7, "z": 2}, {"x": 1, "y": 5, "z": 6}, {"x": 7, "y": 2, "z": 1}],
+             }
+for i in test_data:
+    t = []
+    for j in test_data[i]:
+        tt = [j['x'], j['y'], j['z']]
+        t.append(tt)
+    res.append(t)
 
 print(res)
+
+res2 = np.array(res)
+print(res2)
+print(res2.shape)
